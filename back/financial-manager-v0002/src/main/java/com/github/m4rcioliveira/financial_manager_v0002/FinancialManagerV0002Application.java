2e6a0b77ebe547184820e28224145fbda267e547
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2026-04-24 09:33:15 -0300

    commmit tudo

---
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/FinancialManagerV0002Application.java
@@ -0,0 +1,13 @@
+package com.github.m4rcioliveira.financial_manager_v0002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagerV0002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagerV0002Application.class, args);
+	}
+
+}
---
+++ back/financial-manager-v0002/src/test/java/com/github/m4rcioliveira/financial_manager_v0002/FinancialManagerV0002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m4rcioliveira.financial_manager_v0002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagerV0002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
