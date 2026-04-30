8abc21f112018636139d45b4d6a5b5fc2d5451df
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2026-04-30 10:28:15 -0300

    modelagem base do sistema financial

---
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/enums/CategoriaEnum.java
@@ -0,0 +1,7 @@
+package com.github.m4rcioliveira.financial_manager_v0002.enums;
+
+public enum CategoriaEnum {
+
+    OUTRAS;
+
+}
---
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/model/Despesa.java
@@ -0,0 +1,47 @@
+package com.github.m4rcioliveira.financial_manager_v0002.model;
+
+import com.github.m4rcioliveira.financial_manager_v0002.enums.CategoriaEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Despesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String valor;
+
+    private String fixa;
+
+    private String qtdParcelas;
+
+    private String parcelada;
+
+    private LocalDateTime dataVencimento;
+
+    @Enumerated(EnumType.STRING)
+    private CategoriaEnum categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "fatura_id")
+    private Fatura fatura;
+
+
+}
