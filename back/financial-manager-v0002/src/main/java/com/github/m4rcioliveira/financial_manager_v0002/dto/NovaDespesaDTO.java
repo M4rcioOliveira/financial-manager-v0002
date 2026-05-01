6634be50711733f1fa96251b08ab6fee48e5f981
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2026-05-01 13:22:39 -0300

    removendo atributos não usados

--- back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/dto/NovaDespesaDTO.java
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/dto/NovaDespesaDTO.java
@@ -4,7 +4,6 @@ import com.github.m4rcioliveira.financial_manager_v0002.enums.CategoriaEnum;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public record NovaDespesaDTO(
 
@@ -17,8 +16,6 @@ public record NovaDespesaDTO(
         Boolean fixa,
 
         Integer qtdParcelas,
-
-        Boolean parcelada,
 
         LocalDate dataVencimento,
 
--- back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/model/Despesa.java
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/model/Despesa.java
@@ -40,8 +40,6 @@ public class Despesa {
 
     private Integer qtdParcelas;
 
-    private Boolean parcelada;
-
     private LocalDate dataVencimento;
 
     private LocalDateTime dataPagamento;
