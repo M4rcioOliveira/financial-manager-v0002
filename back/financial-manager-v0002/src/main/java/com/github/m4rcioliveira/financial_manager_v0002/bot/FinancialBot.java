146b5f4249f395f74d9a2189685e247c6e65d2f3
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2026-05-02 11:10:04 -0300

    comentando para manter no historico pdf enviado a partir do resource

--- back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/bot/FinancialBot.java
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/bot/FinancialBot.java
@@ -1,22 +1,19 @@
 package com.github.m4rcioliveira.financial_manager_v0002.bot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-import java.io.InputStream;
-
 @Component
+@Slf4j
 public class FinancialBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
 
     private final String token;
@@ -45,6 +42,8 @@ public class FinancialBot implements SpringLongPollingBot, LongPollingSingleThre
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
 
+            log.info("Chat ID: {}", chat_id);
+
             SendMessage message = SendMessage.builder()
                     .chatId(chat_id)
                     .text(message_text)
@@ -59,23 +58,24 @@ public class FinancialBot implements SpringLongPollingBot, LongPollingSingleThre
     }
 
 
-    public void enviarPdfDoResources(Long chatId) {
+    //Pega do resources
+//    public void enviarPdfDoResources(Long chatId) {
+//
+//        try {
+//            ClassPathResource resource = new ClassPathResource("pdf/envioteste.pdf");
+//            InputStream inputStream = resource.getInputStream();
+//
+//            SendDocument sendDocument = SendDocument.builder()
+//                    .chatId(chatId.toString())
+//                    .document(new InputFile(inputStream, "envioteste.pdf"))
+//                    .caption("PDF de teste 📄")
+//                    .build();
+//
+//            telegramClient.execute(sendDocument);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            ClassPathResource resource = new ClassPathResource("pdf/envioteste.pdf");
-            InputStream inputStream = resource.getInputStream();
 
-            SendDocument sendDocument = SendDocument.builder()
-                    .chatId(chatId.toString())
-                    .document(new InputFile(inputStream, "envioteste.pdf"))
-                    .caption("PDF de teste 📄")
-                    .build();
-
-            telegramClient.execute(sendDocument);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-}}
+}
