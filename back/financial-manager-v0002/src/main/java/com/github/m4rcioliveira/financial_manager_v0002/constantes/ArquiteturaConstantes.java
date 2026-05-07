6f8e05aa81acdecbbf805022d033a381f23968f7
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2026-05-07 10:50:57 -0300

    implementação do jwt

---
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/constantes/ArquiteturaConstantes.java
@@ -0,0 +1,9 @@
+package com.github.m4rcioliveira.financial_manager_v0002.constantes;
+
+public class ArquiteturaConstantes {
+
+    private ArquiteturaConstantes(){}
+
+    public static final String ZONE_ID_SP = "America/Sao_Paulo";
+
+}
---
+++ back/financial-manager-v0002/src/main/java/com/github/m4rcioliveira/financial_manager_v0002/security/service/JwtTokenService.java
@@ -0,0 +1,62 @@
+package com.github.m4rcioliveira.financial_manager_v0002.security.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.github.m4rcioliveira.financial_manager_v0002.constantes.ArquiteturaConstantes;
+import com.github.m4rcioliveira.financial_manager_v0002.security.details.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@Service
+public class JwtTokenService {
+
+    @Value("${financial.security.jwt.token}")
+    private String secretKey;
+
+    @Value("${financial.security.jwt.issuer}")
+    private String issuer;
+
+    public String generateToken(UserDetailsImpl user) {
+        try {
+            // Define o algoritmo HMAC SHA256 para criar a assinatura do token passando a chave secreta definida
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.create()
+                    .withIssuer(issuer) // Define o emissor do token
+                    .withIssuedAt(creationDate()) // Define a data de emissão do token
+                    .withExpiresAt(expirationDate()) // Define a data de expiração do token
+                    .withSubject(user.getUsername()) // Define o assunto do token (neste caso, o nome de usuário)
+                    .sign(algorithm); // Assina o token usando o algoritmo especificado
+        } catch (JWTCreationException exception){
+            throw new JWTCreationException("Erro ao gerar token.", exception);
+        }
+    }
+
+    public String getSubjectFromToken(String token) {
+        try {
+            // Define o algoritmo HMAC SHA256 para verificar a assinatura do token passando a chave secreta definida
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.require(algorithm)
+                    .withIssuer(issuer) // Define o emissor do token
+                    .build()
+                    .verify(token) // Verifica a validade do token
+                    .getSubject(); // Obtém o assunto (neste caso, o nome de usuário) do token
+        } catch (JWTVerificationException exception){
+            throw new JWTVerificationException("Token inválido ou expirado.");
+        }
+    }
+
+    private Instant creationDate() {
+        return ZonedDateTime.now(ZoneId.of(ArquiteturaConstantes.ZONE_ID_SP)).toInstant();
+    }
+
+    private Instant expirationDate() {
+        return ZonedDateTime.now(ZoneId.of(ArquiteturaConstantes.ZONE_ID_SP)).plusHours(4).toInstant();
+    }
+
+}
